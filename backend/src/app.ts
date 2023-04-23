f0db5ffda4940d1bded550715cc1599e1c600682
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-04-23 17:27:59 +0300

    Installed zod and setting up env variables with types

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,7 +22,7 @@ app.get('/health', (req, res) => {
   res.send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Backend running on port: ${PORT}`);
