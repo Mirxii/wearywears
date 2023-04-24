33470b49382df09d2446e37ba206bad0c33e029b
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-04-24 22:35:35 +0300

    Added react-router with basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -11,6 +17,13 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
