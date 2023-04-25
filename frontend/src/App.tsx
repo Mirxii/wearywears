2e77f1a44167bc69b4a80550d1c2fc7a0fa52a09
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-04-25 21:30:35 +0300

    Moved themeprovider from app to main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -8,25 +5,16 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/navbar/Navbar';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
