d2511464d5e91339b2403fa956161639a5d7bef6
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-06 16:39:48 +0300

    Tests for POST Listings endpoint

--- backend/src/__tests__/listings.test.ts
+++ backend/src/__tests__/listings.test.ts
@@ -1,5 +1,7 @@
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect, beforeAll, afterAll } from '@jest/globals';
 import supertest from 'supertest';
+import bcrypt from 'bcrypt';
+import prisma from '../db/prismaClient';
 import app from '../app';
 
 describe('GET Listings endpoint', () => {
@@ -17,7 +19,7 @@ describe('GET Listings endpoint', () => {
     expect(response.body).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
-          id: 'f45a2cf6-ccbc-45ea-9d76-f96327ac229a',
+          id: 'ce31776d-0572-4a1e-9896-70a9f1ce2ed2',
           title: 'Brown jacket',
           description: 'Brown jacket for sale',
           location: 'Helsinki',
@@ -25,7 +27,7 @@ describe('GET Listings endpoint', () => {
             'https://beamhill.fi/wp-content/uploads/2023/02/Lemaire-BOXY-JACKET-Dark-Brown.jpg',
           price: 20,
           category: 'JACKETS',
-          postedById: '94b20cbf-2352-47b2-8c41-a927b1b20e9a',
+          postedById: expect.any(String),
         }),
       ])
     );
@@ -35,7 +37,7 @@ describe('GET Listings endpoint', () => {
 describe('GET Listings by id endpoint', () => {
   test('should return status code 200 if listing exists', (done) => {
     supertest(app)
-      .get('/api/listings/f45a2cf6-ccbc-45ea-9d76-f96327ac229a')
+      .get('/api/listings/ce31776d-0572-4a1e-9896-70a9f1ce2ed2')
       .expect(200)
       .end(done);
   });
@@ -46,14 +48,14 @@ describe('GET Listings by id endpoint', () => {
 
   test('should return json data', async () => {
     const response = await supertest(app)
-      .get('/api/listings/f45a2cf6-ccbc-45ea-9d76-f96327ac229a')
+      .get('/api/listings/ce31776d-0572-4a1e-9896-70a9f1ce2ed2')
       .set('Accept', 'application/json');
 
     expect(response.status).toEqual(200);
     expect(response.type).toEqual('application/json');
     expect(response.body).toEqual(
       expect.objectContaining({
-        id: 'f45a2cf6-ccbc-45ea-9d76-f96327ac229a',
+        id: 'ce31776d-0572-4a1e-9896-70a9f1ce2ed2',
         title: 'Brown jacket',
         description: 'Brown jacket for sale',
         location: 'Helsinki',
@@ -61,7 +63,94 @@ describe('GET Listings by id endpoint', () => {
           'https://beamhill.fi/wp-content/uploads/2023/02/Lemaire-BOXY-JACKET-Dark-Brown.jpg',
         price: 20,
         category: 'JACKETS',
-        postedById: '94b20cbf-2352-47b2-8c41-a927b1b20e9a',
+        postedById: expect.any(String),
+      })
+    );
+  });
+});
+
+describe('POST Listings endpoint', () => {
+  let user_id: string;
+  let user_token: string;
+
+  beforeAll(async () => {
+    const response = await supertest(app)
+      .post('/api/users/signup')
+      .send({
+        name: 'Test User',
+        email: 'user@test.com',
+        password: await bcrypt.hash('password', 10),
+      });
+
+    user_id = response.body.id;
+    user_token = response.body.token;
+  });
+
+  afterAll(async () => {
+    await prisma.listing.deleteMany({
+      where: {
+        postedById: String(user_id),
+      },
+    });
+
+    await prisma.user.delete({
+      where: {
+        id: String(user_id),
+      },
+    });
+  });
+
+  test('should create a new listing', async () => {
+    const response = await supertest(app)
+      .post('/api/listings')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({
+        title: 'Test Jacket',
+        description: 'This is a test listing for jacket',
+        price: 100,
+        location: 'Test Location',
+        category: 'JACKETS',
+        image: 'https://picsum.photos/200',
+        postedById: user_id,
+      });
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        title: 'Test Jacket',
+        description: 'This is a test listing for jacket',
+        price: 100,
+        location: 'Test Location',
+        category: 'JACKETS',
+        image: 'https://picsum.photos/200',
+        postedById: user_id,
+      })
+    );
+  });
+
+  test('should return status code 400 if title is missing', async () => {
+    const response = await supertest(app)
+      .post('/api/listings')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${user_token}`)
+      .send({
+        description: 'This is a test listing for jacket',
+        price: 100,
+        location: 'Test Location',
+        category: 'JACKETS',
+        image: 'https://picsum.photos/200',
+        postedById: user_id,
+      });
+
+    expect(response.status).toEqual(400);
+    expect(response.type).toEqual('application/json');
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'Required',
       })
     );
   });
